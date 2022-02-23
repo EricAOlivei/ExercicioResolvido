7feeaf9f5368ccb910965b2ea3df888f8f611208
EricAOlivei <oliveira_eric@educadventista.org>
2022-02-23 15:15:37 -0300

    Criação das classes e subclasses

---
+++ ExercicoResolvido/Entities/Departmant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicoResolvido.Entities
+{
+    internal class Departmant
+    {
+        public string Name { get; set; }
+
+        public Departmant()
+        {
+        }
+
+        public Departmant(string name)
+        {
+            Name = name;
+        }
+    }
+}
